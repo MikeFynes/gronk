ffef5c5ca86478320ddee104f9f50bddd86c9e00
michael <mikefynes@gmail.com>
2019-09-23 00:36:36 +0300

    remove useless bean

--- fronk/src/main/kotlin/com/fynes/fronk/FronkApplication.kt
+++ fronk/src/main/kotlin/com/fynes/fronk/FronkApplication.kt
@@ -1,11 +1,8 @@
 package com.fynes.fronk
 
-import com.fynes.kronk.OpenHoursFormatterService
-import com.fynes.kronk.OpenHoursFormatterServiceImpl
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 open class FronkApplication
@@ -13,10 +10,5 @@ open class FronkApplication
 fun main(args: Array<String>) {
     runApplication<FronkApplication>(*args) {
         setBannerMode(Banner.Mode.OFF)
-
-        @Bean
-        fun getOpenHoursFormatterService(): OpenHoursFormatterService {
-            return OpenHoursFormatterServiceImpl()
-        }
     }
 }
